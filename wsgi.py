fbf94c0cfe1c4f56e82c244b9e4d00a8166cc30f
JamieWilliamsHackIT <mail@jamiewilliams.co.uk>
2020-01-25 20:46:18 +0000

    :wrench: Change wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,4 @@
 import sys
-import logging
-from app import app as application
+sys.path.insert(0, '/var/www/html/ec2-femm-instance')
 
-activate_this = '/home/ubuntu/ec2-femm-instance/venv/bin/activate_this.py'
-
-with open(activate_this) as f:
-    exec(f.read(), dict(__file__=activate_this))
-
-logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0, "/var/www/html/ec2-femm-instance/")
+from flaskapp import app as application
