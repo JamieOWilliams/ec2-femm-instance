50128d07b73e229b4b32887f51fd47921a6e39b6
JamieWilliamsHackIT <mail@jamiewilliams.co.uk>
2020-01-26 17:23:46 +0000

    :zap: Correct paths

--- femm.py
+++ femm.py
@@ -12,9 +12,9 @@ class Interface:
         os.system(command)
 
     def mesh(self):
-        self._run_command(f'./fmesher {self._filename}.fem')
+        self._run_command(f'./fem/fmesher {self._filename}.fem')
 
     def solve(self):
-        self._run_command(f'./fsolver {self._filename}')
+        self._run_command(f'./fem/fsolver {self._filename}')
         self.ans_filename = f'{self._filename}.ans'
         return self.ans_filename
