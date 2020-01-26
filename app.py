c3bbb9bcee7c44094ff2c0d7a0cab3ec643b4857
JamieWilliamsHackIT <mail@jamiewilliams.co.uk>
2020-01-26 17:02:53 +0000

    :zap: Update code

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+import copy
+import os
+
+from flask import Flask, request, send_from_directory
+from femm import Interface
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './fem'
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/solve', methods=['POST'])
+def solve():
+    """Accept a POST request, mesh and solve the problem."""
+    file = request.files['file']
+    filename = file.filename
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    interface = Interface(filename=os.path.splitext(filename)[0])
+    interface.mesh()
+    ans_filename = interface.solve()
+    return send_from_directory(app.config['UPLOAD_FOLDER'], ans_filename, mimetype='application/octet-stream')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0:80', port=80)
---
+++ femm.py
@@ -0,0 +1,20 @@
+import os
+
+
+class Interface:
+    ans_filename = None
+
+    def __init__(self, filename=None):
+        self._filename = filename
+
+    @staticmethod
+    def _run_command(command):
+        os.system(command)
+
+    def mesh(self):
+        self._run_command(f'./fmesher {self._filename}.fem')
+
+    def solve(self):
+        self._run_command(f'./fsolver {self._filename}')
+        self.ans_filename = f'{self._filename}.ans'
+        return self.ans_filename
